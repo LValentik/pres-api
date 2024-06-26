4803218e2f961add0fe98c9c23b691fde3f93684
Lukáš Valentík <113976719+LValentik@users.noreply.github.com>
2024-06-26 09:28:38 +0200

    Add files via upload

---
+++ presloviny-api-strapi/src/api/manager-email/controllers/manager-email.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manager-email controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::manager-email.manager-email');
---
+++ presloviny-api-strapi/src/api/manager-email/routes/manager-email.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manager-email router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::manager-email.manager-email');
